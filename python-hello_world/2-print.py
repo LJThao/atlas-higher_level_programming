f38df5bfaf911dd56d00549e83e297f0a1977ae9
LJThao <yengfong.thao@atlasschool.com>
2024-04-30 21:41:39 +0000

    adjust comment

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Write a Python script that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+# Print exactly this using Python: "Programming is like building a multilingual puzzle, followed by a new line.
 
 puzzle = "\"Programming is like building a multilingual puzzle"
 
