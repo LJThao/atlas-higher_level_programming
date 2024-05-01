fa657d6b4032c7efe7582ba8f3d64d3d31aedceb
LJThao <yengfong.thao@atlasschool.com>
2024-05-01 01:53:08 +0000

    pycodestyle valid

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Print 3 times a string stored in the variable str, followed by its first 9 characters.
+# Print 3 * a string stored in the variable str, followed by its first 9 chars.
 str = "Holberton School"
 print(str * 3)
 print(str[:9])
