a0aeeebda0498ac662655df95edb40bc768d52e1
LJThao <yengfong.thao@atlasschool.com>
2024-05-07 21:26:09 +0000

    task 0 - added code

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    
+    num = 0
+    for element in range(x):
+        try:
+            print(f"{my_list[element]}", end="")
+            num += 1
+        except IndexError:
+            break
+        print()
+        return(num)
