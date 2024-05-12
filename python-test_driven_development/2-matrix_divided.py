46978ca32d119ad66925da3f672653d0a97e4419
LJThao <yengfong.thao@atlasschool.com>
2024-05-12 20:19:39 +0000

    added code to main files

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Divide a matrix"""
+
+
+def matrix_divided(matrix, div):
+    """Write a function that divides all element"""
+
+    if not type(div) in (int, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    terr_msg = "matrix must be a matrix (list of lists) of integers/floats"
+
+    if not matrix or not isinstance(matrix, list):
+        raise TypeError(terr_msg)
+
+    _len = 0
+    serr_msg = "Each row of the matrix must have the same size"
+
+    for elems in matrix:
+        if not elems or not isinstance(elems, list):
+            raise TypeError(terr_msg)
+
+        if _len != 0 and len(elems) != _len:
+            raise TypeError(serr_msg)
+
+        for num in elems:
+            if not type(num) in (int, float):
+                raise TypeError(terr_msg)
+
+        _len = len(elems)
+
+    r = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
+    return (r)
