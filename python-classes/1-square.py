cf2132335fc4b94cc3fbe0bf92ea365ab7590bb9
LJThao <yengfong.thao@atlasschool.com>
2024-05-12 19:10:11 +0000

    changed code

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,8 +5,9 @@ Write a class Square
 
 
 class Square:
-    """Private attribute"""
+    """Class to define a square"""
     
     def __init__(self, size):
+        """Private attribute"""
 
     self.___size = size
