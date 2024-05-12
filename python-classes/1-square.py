ded46f98a271e411d3164a498639d9fe14083b7d
LJThao <yengfong.thao@atlasschool.com>
2024-05-12 19:11:43 +0000

    fix errors

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""
-Write a class Square
-"""
+""" Write a class Square """
 
 
 class Square:
     """Class to define a square"""
-    
+
     def __init__(self, size):
         """Private attribute"""
 
-    self.___size = size
+    self.__size = size
