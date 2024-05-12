4023bebbd757533e09d16b24443222942b88fbbe
LJThao <yengfong.thao@atlasschool.com>
2024-05-12 19:59:54 +0000

    made changes to code

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -24,7 +24,7 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
-        
+
         if value < 0:
             raise ValueError('size must be >= 0')
 
--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,4 +1,4 @@
-#1/usr/bin/python3
+#!/usr/bin/python3
 """
 Coordinates of a square: Write a class Square
 """
