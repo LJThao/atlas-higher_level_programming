ec9a227f507b32aeaf645b31a0cf51317b01b7c5
LJThao <yengfong.thao@atlasschool.com>
2024-05-13 04:14:03 +0000

    made changes to test file

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
+"""Max integer - Unittesy
 """
 
 import unittest
@@ -7,22 +7,25 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def max_end(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([13, -4, 300, 35]), 300)
         
     def max_begin(self):
         self.assertEqual(max_integer([4, 3, 2, 1]), 4)
         
     def max_mid(self):
-        self.assertEqual(max_integer([1, 4, 2, 3]), 4)
+        self.assertEqual(max_integer([1, 4, 2, 3]), 2)
         
     def one_int(self):
-        self.assertEqual(max_integer([4]), 4)
+        self.assertEqual(max_integer([5]), 5)
     
     def same_int(self):
-        self.assertEqual(max_integer([5, 5, 5, 5, 5]), 5)
+        self.assertEqual(max_integer([50, 2024, 2024]), 2024)
         
     def no_int(self):
         self.assertEqual(max_integer([]), None)
         
     def neg_int(self):
-        self.assertEqual(max_integer([-4, -5, -6, -7, -8]), -4)
+        self.assertEqual(max_integer([-1, -2, -3, -4, -5]), -4)
+    
+    def zero_int(self):
+        self.assertEqual(max_integer([0, 0, 0, 0]), 0)
