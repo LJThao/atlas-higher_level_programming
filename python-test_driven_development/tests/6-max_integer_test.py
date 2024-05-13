c9fbe4109f51c9c6876d690b417150d97a0bbb03
LJThao <yengfong.thao@atlasschool.com>
2024-05-13 03:57:33 +0000

    added test file t6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python3
-"""Unittest"""
+"""Module to find the max integer in a list
+"""
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def max_end(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        
+    def max_begin(self):
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+        
+    def max_mid(self):
+        self.assertEqual(max_integer([1, 4, 2, 3]), 4)
+        
+    def one_int(self):
+        self.assertEqual(max_integer([4]), 4)
+    
+    def same_int(self):
+        self.assertEqual(max_integer([5, 5, 5, 5, 5]), 5)
+        
+    def no_int(self):
+        self.assertEqual(max_integer([]), None)
+        
+    def neg_int(self):
+        self.assertEqual(max_integer([-4, -5, -6, -7, -8]), -4)
