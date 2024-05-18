2579e9307175815e863a6169eee8dce50ded9225
LJThao <yengfong.thao@atlasschool.com>
2024-05-18 20:42:01 +0000

    added code to t2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This funcion returns True if the object
+is exactly an instance of the specified class;
+otherwise False"""
+
+
+def is_same_class(obj, a_class):
+    """function used for returning the object"""
+
+    return (type(obj) == a_class)
