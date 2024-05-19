751eed767680cc97ed6031e0c15b7d58f34aab04
LJThao <yengfong.thao@atlasschool.com>
2024-05-19 05:19:52 +0000

    added code to t0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This function will read a text
+file (UTF8) and print to stdout"""
+
+
+def read_file(filename=""):
+    """Function to read UTF8 file"""
+
+    with open(filename) as f:
+        print(f.read())
