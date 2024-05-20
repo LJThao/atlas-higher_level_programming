adeea608aaedf7bfad4d2d5a7fd5102b09596207
LJThao <yengfong.thao@atlasschool.com>
2024-05-20 05:25:03 +0000

    added code to t9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -4,7 +4,25 @@ class Rectangle that inherits from
 BaseGeometry (7-base_geometry.py)"""
 
 
-class Rectangle():
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
     """Class Rectangle"""
 
-    
+    def __init__(self, width, height):
+        """Function that validates"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """Function that return a string the following
+        rectangle description"""
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
