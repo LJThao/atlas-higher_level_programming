16333fb2d9924d333aaf1a62b20fae5a5a645b3e
LJThao <yengfong.thao@atlasschool.com>
2024-05-20 05:37:34 +0000

    added code to t10 and t11

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,7 +4,15 @@ class Square that inherits from
 Rectangle (9-rectangle.py)"""
 
 
-class Square():
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
     """Class Square"""
 
-    
+    def __init__(self, size):
+        """Function that validates"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -4,6 +4,24 @@ Square that inherits from Rectangle
 (9-rectangle.py)"""
 
 
-class Square():
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
     """Class Square"""
-    
+
+    def __init__(self, size):
+        """Function that validates"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def __str__(self):
+        """Function to print and return str()
+        and the square description"""
+
+        return ("[Square] {}/{}".format(self.__size, self.__size))
