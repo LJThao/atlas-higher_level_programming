eec098c8030f7fc29c414f34de63526b464dfe0a
LJThao <yengfong.thao@atlasschool.com>
2024-05-20 04:21:23 +0000

    PEP8 valid

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -13,11 +13,13 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self, attrs=None):
         """Function to retrieve a dictionary representation
         of a Student instance"""
 
         if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {attr: getattr(self, attr) for attr in attrs
+            if hasattr(self, attr)}
+
         return (self.__dict__)
