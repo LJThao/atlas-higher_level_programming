2f2d38304ca9ce728f62b691807ebcdebade8a59
LJThao <yengfong.thao@atlasschool.com>
2024-05-20 05:07:14 +0000

    added code to t7 and test file

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This function will write a
-class Base Geometry (based on
+class BaseGeometry (based on
 6-base_geometry.py)"""
 
 
@@ -8,7 +8,13 @@ class BaseGeometry():
     """Class BaseGeometry"""
 
     def area(self):
+        raise Exception("areas() is not implemented")
 
-    
     def integer_validator(self, name, value):
-    
+        """Function that validates a value"""
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
