ee987f60643827a0c8c5f1b36a3bd162e1a4ffe1
LJThao <yengfong.thao@atlasschool.com>
2024-05-25 21:50:05 +0000

    added code to t1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,7 +2,14 @@
 """Creating Class Base Model"""
 
 
-import json
-
-
 class Base:
+    """Class Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Function that initializes"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
