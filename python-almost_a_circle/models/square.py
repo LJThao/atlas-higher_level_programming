028529a1342b20cc39101e9da75ad7ed72498729
LJThao <yengfong.thao@atlasschool.com>
2024-05-26 04:53:40 +0000

    added init() and str() function to square file

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Created Class Square Module"""
+
+
+class Square(Rectange):
+    """Class Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Function that initializes a new instance"""
+        super().__init__(size, size, x, y, id)
+    
+    def __str__(self):
+        """Returning format string
+        [Square] (<id>) <x>/<y> - <size>"""
+        return (("[{}] ({}) {}/{} - {}".format(type(self).__name__,
+        self.id, self.x, self.y, self.width)))
