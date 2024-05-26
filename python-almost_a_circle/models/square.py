1d6c276f0f5e9e737fe71de7946af61c5ba6deb4
LJThao <yengfong.thao@atlasschool.com>
2024-05-26 20:19:31 +0000

    change return

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -43,5 +43,4 @@ class Square(Rectangle):
     def to_dictionary(self):
        """Function that returns the dictionary
        representation of Rectangle"""
-       
-       return ({"id": self.id, "size": self.size, "x": self.x, "y": self.y})
+       return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
