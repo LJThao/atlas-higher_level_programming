778b14784cd2b52fbbdd583fead179fa7fe26ae3
LJThao <yengfong.thao@atlasschool.com>
2024-05-26 18:37:07 +0000

    corrected typo and remove import function for now

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -2,10 +2,7 @@
 """Created Class Square Module"""
 
 
-from models.base import Base
-
-
-class Square(Rectange):
+class Square(Rectangle):
     """Class Square"""
     def __init__(self, size, x=0, y=0, id=None):
         """Function that initializes a new instance"""
