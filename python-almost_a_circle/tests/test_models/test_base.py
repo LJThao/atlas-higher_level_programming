4b7fbdb4bd99a5de288304093688b4a74c12a36a
LJThao <yengfong.thao@atlasschool.com>
2024-05-27 20:34:49 +0000

    test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -42,18 +42,5 @@ class TestBase(unittest.TestCase):
         self.assertEqual(json_list, [expected_dict])
         self.assertIsInstance(json_dictionary, str)
 
-# Tests for Rectangle Module
-class TestRectangle(unittest.TestCase):
-
-    def test_rectangle_creation(self):
-        r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 1)
-
-        r2 = Rectangle(2, 10)
-        self.assertEqual(r2.id, 2)
-
-        r3 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(r3.id, 12)
-
 if __name__ == '__main__':
     unittest.main()
