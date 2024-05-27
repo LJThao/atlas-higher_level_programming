bdbb57de3ace891f527a5b3270fae99e4e0483dc
LJThao <yengfong.thao@atlasschool.com>
2024-05-27 20:30:53 +0000

    test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -44,3 +44,20 @@ class TestBase(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+
+# Tests for Rectangle Module
+class TestRectangle(unittest.TestCase):
+
+    def test_rectangle_creation(self):
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
+
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.id, 2)
+
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
+
+if __name__ == '__main__':
+    unittest.main()
