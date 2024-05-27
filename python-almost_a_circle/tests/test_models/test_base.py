d3e79b8309edb8dfc510cacc50509396e974e067
LJThao <yengfong.thao@atlasschool.com>
2024-05-27 19:48:51 +0000

    added test_base_json_string () to test file

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -4,9 +4,10 @@
 
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 
-# Task 1 - Base Class Test
+# Tests for Base Module
 class TestBase(unittest.TestCase):
     """Class to test the Base Class"""
     def test_ids(self):
@@ -25,6 +26,16 @@ class TestBase(unittest.TestCase):
     def test_specific_id(self):
         b4 = Base(12)
         self.assertEqual(b4.id, 12)
+
+    def test_base_json_string(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+
+        self.assertEqual(dictionary, {'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8})
+        self.assertIsInstance(dictionary, dict)
+        self.assertEqual(json_dictionary, '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]')
+        self.assertIsInstance(json_dictionary, str)
 
 if __name__ == "__main__":
     unittest.main()
