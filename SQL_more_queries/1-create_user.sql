24a6d907ff93991b65e02f526b0d59c244240a22
LJThao <yengfong.thao@atlasschool.com>
2024-06-16 22:29:29 +0000

    changing comment formats

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
--- Creates the MySQL server user user_0d_1.
+-- Task 1. Creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-# create the user if it does not exists
+-- create the user if it does not exists
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-# granting all privileges to the user
+-- granting all privileges to the user
