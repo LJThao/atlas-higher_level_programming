3ddd9e70a30e5f367b47fffa87c81b90b2ddb56e
LJThao <yengfong.thao@atlasschool.com>
2024-06-16 21:43:01 +0000

    made changes and created files

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+# showing user_0d_1 privileges
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
+# showing user_0d_2 privileges
---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- Creates the database hbtn_0d_2 and the user user_0d_2.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+# create database if the database doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+# create a new user with a password if the user doesn't exist
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+# grant the user permission to select data from the database
