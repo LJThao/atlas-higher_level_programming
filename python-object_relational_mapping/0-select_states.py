e1b260946e6b0a6e1f5b4bc855a1e27ff5d11edc
LJThao <yengfong.thao@atlasschool.com>
2024-06-17 05:25:23 +0000

    created files and list all states for t0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+
+
+from sys import argv
+import MySQLdb
+
+
+def connect_mysql():
+    """Connect to MySQL server running on
+    localhost at port 3306"""
+    db = MySQLdb.connect(
+        user=argv[1],
+        passwd=argv[2], 
+        db=argv[3], 
+        host="localhost",
+        port = 3306
+        )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
+if __name__ == '__main__':
+    connect_mysql()
