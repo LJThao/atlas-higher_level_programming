42677768d22b112004027b611c3b024a76add38a
LJThao <yengfong.thao@atlasschool.com>
2024-06-17 00:43:43 +0000

    create force_name table

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- Creates the table force_name on your MySQL server.
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+)
+-- id INT = unique identifier for each record.
+-- name VARCHAR(256) NOT NULL = force name that is provided and cannot have a missing value.
