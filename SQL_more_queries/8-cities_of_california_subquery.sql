5ae518268a72e90c524bc15918dfabcb219cfbcc
LJThao <yengfong.thao@atlasschool.com>
2024-06-18 22:43:38 +0000

    made changes

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,10 @@
 -- Lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT cities.name, cities.id
-FROM cities, states
-WHERE cities.state_id = states.id
-AND states.name = 'California'
+SELECT , cities.id
+FROM cities
+WHERE cities.state_id =
+(
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
 ORDER BY cities.id ASC;
