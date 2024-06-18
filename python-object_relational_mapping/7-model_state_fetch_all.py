f52b5fd6c95ab9b2b280795bdc1969fc37c4e8ab
LJThao <yengfong.thao@atlasschool.com>
2024-06-18 21:03:29 +0000

    created script to list states with an ORM

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Script that lists all the
+State objects from the database
+hbtn_0e_6_usa"""
+
+
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base
+from model_state import State
+
+
+def connect_mysql():
+    """Connect to a MySQL server running
+    on localhost at port 3306 with an ORM"""
+    db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3]
+    )
+
+    engine = create_engine(db)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).order_by(State.id)
+    rows = query.all()
+    for row in rows:
+        print("{:d}: {:s}".format(row.id, row.name))
+
+
+if __name__ == "__main__":
+    connect_mysql()
