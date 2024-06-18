e7fd43ae14dd11cbf268329a374237ef945f0feb
LJThao <yengfong.thao@atlasschool.com>
2024-06-18 02:52:25 +0000

    created State class

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Class definition of a State and an
+instance Base = declarative_base()."""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class that defines the State instances"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
