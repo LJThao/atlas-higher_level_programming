eca13d83ba66b4ea242f971d7bcac2b32a3cdad8
LJThao <yengfong.thao@atlasschool.com>
2024-07-26 21:15:34 +0000

    `semistandard --fix` -- validation

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 
 // Reading the file content
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
-    // Checking for error
-    if (err) {
-        return console.error(`Error: ${err.message}`);
-    }
-    // Printing the file content
-    console.log(data);
+  // Checking for error
+  if (err) {
+    return console.error(`Error: ${err.message}`);
+  }
+  // Printing the file content
+  console.log(data);
 });
